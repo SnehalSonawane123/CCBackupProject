7568ce9d5365f5b44876fbe22715d9c1f5360730
SnehalSonawane123 <snehalsonawane984@gmail.com>
2026-04-24 21:35:13 +0530

    fix: javax.servlet-api 4.0.1

--- src/main/java/com/avcoe/backup/LoginServlet.java
+++ src/main/java/com/avcoe/backup/LoginServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
