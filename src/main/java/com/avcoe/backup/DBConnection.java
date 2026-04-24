4e7ea1ba333df6399857fbe9f2ae83f606d66c6e
SnehalSonawane123 <snehalsonawane984@gmail.com>
2026-04-24 19:57:32 +0530

    fix: connect to Aiven MySQL

--- src/main/java/com/avcoe/backup/DBConnection.java
+++ src/main/java/com/avcoe/backup/DBConnection.java
@@ -3,9 +3,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/s3backupdb";
-    private static final String USER = "root";
-    private static final String PASS = "";
+    private static final String URL = "jdbc:mysql://mysql-2e1563aa-snehalsonawane984-3647.d.aivencloud.com:27386/defaultdb?sslMode=REQUIRED";
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASS = System.getenv("DB_PASS");
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
